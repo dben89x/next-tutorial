cb401ec80f9532664a5aed4a1d2e0e9879b3b270
Doug Bennett <dben89x@gmail.com>
2018-11-28 13:40:22 -0800

    adding dirs to gitignore

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const dev = process.env.NODE_ENV !== 'production'
 const app = next({dev})
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
+app.prepare().then( () => {
   const server = express()
 
   server.get('/p/:id', (req, res) => {
